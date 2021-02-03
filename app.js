ed169a12703cd6e43d63e3ce92e629e3b069c918
Darius Raazi <dariusraazi@icloud.com>
2021-02-03 16:03:54 -0700

    commit for git req

--- app.js
+++ app.js
@@ -5,15 +5,13 @@ console.log(jokeBtn)
 
 // Next, add an event listener to the button variable that will fire a function when the button is clicked. Hint: as a first test, just log a message to yourself when the user clicks the button and don't worry about the API stuff just yet. Baby steps!
 
-
-
 // Now we're ready to add fetch in to the mix. Using fetch and the URL provided above, make a request to the Chuck Norris API and handle the response. Hint: In the body of the first .then() callback function, you'll need to parse the response from the API first. If you don't remember this step from the lesson, review it or do a quick search for "parse JSON response with fetch" or something along those lines.
-const requestUrl = 'https://api.chucknorris.io/jokes/random'
-
-fetch(requestUrl)
+const randomUrl = 'https://api.chucknorris.io/jokes/random'
+fetch(randomUrl)
 .then((responseData) => {
     return responseData.json()
 })
+// Once you've parsed the JSON response you'll need to add a second .then() method to the fetch chain. This is where we can use the data from the API and do a little DOM manipulation to show the user a random joke. Think back to what you know about DOM manipulation and start small. You can do this!
 .then((jsonData) => {
     console.log(jsonData.value)
     jokeBtn.addEventListener('click', function(e) {
@@ -37,5 +35,20 @@ clrBtn.addEventListener('click', function(e){
     container.innerText = ''
 })
 
-// Once you've parsed the JSON response you'll need to add a second .then() method to the fetch chain. This is where we can use the data from the API and do a little DOM manipulation to show the user a random joke. Think back to what you know about DOM manipulation and start small. You can do this!
+// New Fetch for Bonus functionality
+const categoryUrl = 'https://api.chucknorris.io/jokes/categories'
+fetch(categoryUrl)
+.then((responseData) => {
+    return responseData.json()
+})
+.then((jsonData) => {
+    console.log(jsonData.value)
+})
+.catch((error) => {
+    console.error(`there has been an error, ${error}`)
+})
 
+
+for (category in jokeCategories){
+
+}
